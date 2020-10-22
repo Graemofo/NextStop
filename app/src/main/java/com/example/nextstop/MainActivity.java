b324b3675506dd63077eb6154c19d0400ffbfb1f
Graeme Doherty <graemedoherty@Graemes-MacBook-Pro.local>
2020-10-22 10:49:45 +0100

    Update ui colors

--- app/src/main/java/com/example/nextstop/MainActivity.java
+++ app/src/main/java/com/example/nextstop/MainActivity.java
@@ -3,14 +3,15 @@ package com.example.nextstop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,10 +20,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import okhttp3.OkHttpClient;
-import retrofit2.Retrofit;
-import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
-
 public class MainActivity extends AppCompatActivity {
 
     /*
@@ -30,15 +27,20 @@ public class MainActivity extends AppCompatActivity {
     To Do list
     1. Create a list of Station Objects from JSON
     2. Get current location
-    3. Create UI to accept destination (fetch possible destinations)
+    3. Create UI to accept destination (fetch possible destinations) Auto Complete
 
      */
 
+    public ArrayList<String> destinations = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button goButton = findViewById(R.id.goButton);
+        final TextView textView = findViewById(R.id.textView2);
+
 
         String stationsJSON = "";
 
@@ -48,28 +50,52 @@ public class MainActivity extends AppCompatActivity {
             byte[] buffer = new byte[size];
             inputStream.read(buffer);
             stationsJSON = new String(buffer);
-            createStationList(stationsJSON);
+            List<Station> stations = createStationList(stationsJSON);
+            destinations = createDestinations(stations);
             //  Log.d("Json", "onCreate: "+stationsJSON);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        final AutoCompleteTextView editText = findViewById(R.id.autoComplete);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, destinations);
+        editText.setAdapter(adapter);
+
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String value = editText.getText().toString();
+                textView.setText(value);
+            }
+        });
+
     }
 
-    public void createStationList(String json) {
+    public List<Station> createStationList(String json) {
         Type stationType = new TypeToken<ArrayList<Station>>() {
         }.getType();
         List<Station> stations = new Gson().fromJson(json, stationType);
         printStations(stations);
-     //   Log.d("MainActivity", "createStationList: " + stations.size());
+        //   Log.d("MainActivity", "createStationList: " + stations.size());
+        return stations;
     }
 
     public void printStations(List<Station> stations) {
-
         Collections.sort(stations);
-        for(Station station : stations) {
-            System.out.println("Name: "+station.getStationId()+" , "+station.getStationDesc()+": Lon: "+station.getStationLongitude()+" : Lat: "+station.getStationLatitude());
+        for (Station station : stations) {
+            destinations.add(station.getStationDesc());
+            System.out.println("Name: " + station.getStationId() + " , " + station.getStationDesc() + ": Lon: " + station.getStationLongitude() + " : Lat: " + station.getStationLatitude());
         }
     }
+
+    public ArrayList<String> createDestinations(List<Station> stations) {
+        for (Station station : stations) {
+            destinations.add(station.getStationDesc());
+        }
+        return destinations;
+    }
+
 
 }
